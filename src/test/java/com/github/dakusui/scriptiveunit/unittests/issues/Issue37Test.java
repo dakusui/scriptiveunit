b69827f1ac01cae730c0a0b9ba6a2617d6711ca0
Hiroshi Ukai <dakusui@gmail.com>
2019-04-16 07:28:20 +0900

    WIP: Add tests to verify execution order

--- src/test/java/com/github/dakusui/scriptiveunit/unittests/issues/Issue37Test.java
+++ src/test/java/com/github/dakusui/scriptiveunit/unittests/issues/Issue37Test.java
@@ -1,5 +1,6 @@
 package com.github.dakusui.scriptiveunit.unittests.issues;
 
+import com.github.dakusui.scriptiveunit.drivers.actions.Basic;
 import com.github.dakusui.scriptiveunit.examples.Qapi;
 import com.github.dakusui.scriptiveunit.testutils.TestUtils;
 import org.junit.Test;
@@ -11,58 +12,24 @@ import org.junit.runner.Runner;
 import org.junit.runner.notification.RunListener;
 import org.junit.runner.notification.RunNotifier;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Issue37Test {
   public abstract static class Base {
 
     @Test
     public void test() {
+      List<String> out = new LinkedList<>();
+      Basic.setOut(out::add);
       TestUtils.configureScriptNameSystemProperty(getScriptName(), Qapi.class);
       runTests();
+      out.forEach(System.out::println);
     }
 
     void runTests() {
-      RunNotifier runNotifier = new RunNotifier() {
-        public void fireTestStarted(final Description description) {
-          super.fireTestStarted(description);
-//          System.err.println("fireTestStarted :" + description);
-        }
-
-        public void fireTestFinished(final Description description) {
-          super.fireTestFinished(description);
-//          System.err.println("fireTestFinished:" + description);
-        }
-      };
-      runNotifier.addListener(new RunListener() {
-        @Override
-        public void testRunStarted(Description description) {
-          //          System.err.println("  testRunStarted:" + description.getDisplayName());
-        }
-
-        public void testRunFinished(Result result) {
-          //          System.err.println("  testRunFinished:" + result);
-        }
-
-        /**
-         * Called when an atomic test is about to be started.
-         *
-         * @param description the description of the test that is about to be run
-         * (generally a class and method name)
-         */
-        public void testStarted(Description description) throws Exception {
-          //          System.err.println("  testStarted:" + description.getDisplayName());
-        }
-
-        /**
-         * Called when an atomic test has finished, whether the test succeeds or fails.
-         *
-         * @param description the description of the test that just ran
-         */
-        public void testFinished(Description description) throws Exception {
-          //          System.err.println("  testFinished:" + description.getDisplayName());
-        }
-      });
       Runner runner = Request.classes(new Computer(), Qapi.class).getRunner();
-      runner.run(runNotifier);
+      runner.run(new RunNotifier());
       System.err.println(runner.getDescription());
     }
 
