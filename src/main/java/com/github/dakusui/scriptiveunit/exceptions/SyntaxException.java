27dd8adf39f17c6ab51f3d74ec8743a1ca4cf224
Hiroshi Ukai <dakusui@gmail.com>
2019-07-25 04:30:38 +0900

    Clean up import statements in SyntaxException

--- src/main/java/com/github/dakusui/scriptiveunit/exceptions/SyntaxException.java
+++ src/main/java/com/github/dakusui/scriptiveunit/exceptions/SyntaxException.java
@@ -1,14 +1,5 @@
 package com.github.dakusui.scriptiveunit.exceptions;
 
-import com.github.dakusui.scriptiveunit.model.stage.Stage;
-import com.github.dakusui.scriptiveunit.model.statement.Statement;
-import org.codehaus.jackson.JsonNode;
-
-import java.util.Map;
-import java.util.function.Supplier;
-
-import static java.lang.String.format;
-
 public class SyntaxException extends ScriptiveUnitException {
   public SyntaxException(String message) {
     super(message);
